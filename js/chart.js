5c64d2a2f25a9a0f8b40feaf9c3887eb7d23f1b2
Daniel Caruso <infinetnew@gmail.com>
2026-06-18 22:32:54 +0200

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -1,3 +1,5 @@
+let chart = null;
+
 function openChart(symbol) {
 
     document.getElementById("chartTitle").innerText =
@@ -5,6 +7,54 @@ function openChart(symbol) {
 
     document.getElementById("chartModal").style.display =
         "flex";
+
+    const container =
+        document.getElementById("chartContainer");
+
+    container.innerHTML = "";
+
+    if (chart) {
+
+        chart.remove();
+
+    }
+
+    chart =
+        LightweightCharts.createChart(
+            container,
+            {
+                height: 500
+            }
+        );
+
+    const series =
+        chart.addCandlestickSeries();
+
+    series.setData([
+        {
+            time: "2026-06-10",
+            open: 100,
+            high: 110,
+            low: 95,
+            close: 108
+        },
+        {
+            time: "2026-06-11",
+            open: 108,
+            high: 120,
+            low: 105,
+            close: 118
+        },
+        {
+            time: "2026-06-12",
+            open: 118,
+            high: 125,
+            low: 115,
+            close: 121
+        }
+    ]);
+
+    chart.timeScale().fitContent();
 
 }
 
@@ -14,8 +64,9 @@ document.addEventListener("DOMContentLoaded", () => {
         .getElementById("closeChartBtn")
         ?.addEventListener("click", () => {
 
-            document.getElementById("chartModal").style.display =
-                "none";
+            document.getElementById(
+                "chartModal"
+            ).style.display = "none";
 
         });
 
